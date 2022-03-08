c947443e59099a5d8707948a801e3510985842fb
Ferran <ferran.puigmar@gmail.com>
2022-03-08 20:41:05 +0100

    Complete home url

---
+++ src/api/home/controllers/home.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ *  home controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::home.home", ({ strapi }) => ({
+  async find(ctx) {
+    const { query } = ctx;
+
+    const entity = await strapi.entityService.findMany("api::home.home", {
+      ...query,
+      populate: {
+        specialities: {
+          populate: {
+            backgroundImage: true,
+            image: true,
+            specialities: {
+              populate: {
+                image: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
